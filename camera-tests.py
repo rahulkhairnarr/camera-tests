32144ab173f81635c559610924a09d59a4415853
Harsh Murari <hmurari@qwinix.io>
2020-05-21 22:25:37 -0600

    Add more tests

--- camera-tests.py
+++ camera-tests.py
@@ -68,6 +68,21 @@ class CameraTests(object):
         # backlight_compensation: 0 --> 256, default 65
         self._cam_param_test_num('backlight_compensation', 0, 256, 10, 65)
 
+        # Change focus to manual focus first.
+        # focus_absolute: 0 --> 256, default 0
+        self.cam.set_param('focus_auto', 1)
+        self._cam_param_test_num('focus_absolute', 0, 256, 10, 0)
+
+        # Change exposure to manual first.
+        # exposure_absolute: 3 --> 2047, default 166
+        self.cam.set_param('exposure_absolute', 1)
+        self._cam_param_test_num('exposure_absolute', 3, 10, 2047, 166)
+
+        # Change wb_temperature_auto to manual first.
+        # white_balance_temperature: 2800 --> 6500, default 4600
+        self.cam.set_param('white_balance_temperature_auto', 0)
+        self._cam_param_test_num('white_balance_temperature', 2800, 6500, 100, 4600)
+
         # TBD:
         # These need to be set together to take effect.
         # white_balance_temperature_auto & white_balance_temperature
@@ -76,17 +91,18 @@ class CameraTests(object):
 
     def _cam_param_test_num(self, param, min, max, step, default):
         self.cam.update_title('{} TESTS: {} --> {}, Default: {}'.format(param, min, max, default))
-        time.sleep(5)
+        time.sleep(3)
         for val in range(min, max, step):
             print('Changing {} to: {}'.format(param, val))
-            self.cam.update_title('{} TEST: Current {}, Default {}'.format(param, val, default))
+            self.cam.update_title('{}: Current {}, Default {}'.format(param.upper(), val, default))
             self.cam.set_param(param, val)
             time.sleep(1)
 
         print('{} tests complete. Resetting to defaults.')
         self.cam.update_title('{} TEST: Resetting to defaults.'.format(param))
         self.cam.reset_params_to_default()
-        time.sleep(5)
+        time.sleep(3)
+
 
 
 if __name__ == '__main__':
