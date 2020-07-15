fe633ccabc6334a61430702fd304ced8ed12ba63
Harsh <hmurari@qwinix.io>
2020-07-14 19:51:04 -0600

    Multi-camera tests

--- multi-cam-test.py
+++ multi-cam-test.py
@@ -8,13 +8,22 @@ dev1 = '/dev/video1'
 dev2 = '/dev/video2'
 
 
-image_capture_enabled = True
-display_window_enabled = False
+camera_0_enabled = True
+camera_1_enabled = True
+camera_2_enabled = False
+image_capture_enabled = False 
+display_window_enabled = True 
+
 
 def test_fps_simple():
-    video0 = cv2.VideoCapture(dev0)
-    video1 = cv2.VideoCapture(dev1)
-    video2 = cv2.VideoCapture(dev2)
+    if camera_0_enabled:
+        video0 = cv2.VideoCapture(dev0)
+    
+    if camera_1_enabled:
+        video1 = cv2.VideoCapture(dev1)
+
+    if camera_2_enabled:
+        video2 = cv2.VideoCapture(dev2)
 
     # Number of frames to capture
     num_frames = 120
@@ -28,18 +37,28 @@ def test_fps_simple():
     while True:
         frame_idx+=1
 
-        ret, frame0 = video0.read()
-        ret, frame1 = video1.read()
-        ret, frame2 = video2.read()
+        if camera_0_enabled:
+            ret, frame0 = video0.read()
+        
+        if camera_1_enabled:
+            ret, frame1 = video1.read()
+
+        if camera_2_enabled:
+            ret, frame2 = video2.read()
 
         if frame_idx >= num_frames:
             break
 
         if display_window_enabled:
             try:
-                cv2.imshow('Camera0', frame0)
-                cv2.imshow('Camera1', frame1)
-                cv2.imshow('Camera2', frame2)
+                if camera_0_enabled:
+                    cv2.imshow('Camera0', frame0)
+                
+                if camera_1_enabled:
+                    cv2.imshow('Camera1', frame1)
+
+                if camera_2_enabled:
+                    cv2.imshow('Camera2', frame2)
 
                 if cv2.waitKey(10) == 27:
                     break  # esc to quit
@@ -48,9 +67,12 @@ def test_fps_simple():
                 pass
 
         if image_capture_enabled and frame_idx % 10 == 0:
-            cv2.imwrite("0-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg', frame0)
-            cv2.imwrite("1-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg', frame1)
-            cv2.imwrite("2-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg', frame2)
+            if camera_0_enabled:
+                cv2.imwrite("0-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg', frame0)
+            if camera_1_enabled:
+                cv2.imwrite("1-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg', frame1)
+            if camera_2_enabled:
+                cv2.imwrite("2-" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '.jpg', frame2)
 
     # End time
     end = time.time()
@@ -64,9 +86,14 @@ def test_fps_simple():
     print("Estimated frames per second : {:.2f}".format(fps))
 
     # Release video
-    video0.release()
-    video1.release()
-    video2.release()
+    if camera_0_enabled:
+        video0.release()
+    
+    if camera_1_enabled:
+        video1.release()
+
+    if camera_2_enabled:
+        video2.release()
 
 if __name__ == '__main__':
     test_fps_simple()
